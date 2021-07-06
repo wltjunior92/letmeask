d3878877f929d94a9346c658dc6fcd77a435a822
Junior <wlt.junior92@gmail.com>
2021-07-06 15:57:47 -0300

    Add dinamic user avatar on page Room

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import { ButtonHTMLAttributes } from 'react';
+
+import '../styles/button.scss';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isOutlined?: boolean
+};
+
+export function Button({ isOutlined = false, ...rest }: ButtonProps) {
+  return (
+    <button
+      className={`button ${isOutlined ? 'outlined' : ''}`}
+      {...rest}
+    />
+  );
+}
